d5aa655c090ee3fa90cd503fa54f5a005dc26489
Chesley Tan <chesleytan97@gmail.com>
2015-04-22 12:19:21 -0400

    Add alternate solution to Python 1; Add withheld.sh script

--- Python-1_50/py1.py
+++ Python-1_50/py1.py
@@ -9,4 +9,13 @@ exec((lambda p,y:(lambda o,b,f:re.sub(o,b,f))
 '''
 >>> "e24zdjNyX3VzZV9FVkFMfQ==".decode("base64")
 '{n3v3r_use_EVAL}'
+
+An alternate solution is to add the keyword 'not' after 'if' in the eval string
+as such:
+"0|number|e24zdjNyX3VzZV9FVkFMfQ|input|randint|flag|import|base64|print|correctly|a|b64decode|thinking|random|you|Enter|guess|from|give|else|the|int|ll|If|of|I|if not|Incorrect|10792579157912751097616189106168175192741851286517|m"
+
+>> python py1.py
+I'm thinking of a number. If you guess correctly, I'll give you the flag. Enter a number: 1
+{n3v3r_use_EVAL}
+
 '''
