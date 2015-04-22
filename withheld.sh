b283b5d793467f17cb08815e2c8f3b39bbf86ab9
Danny Qiu <dqiu55@gmail.com>
2015-04-22 17:54:03 -0400

    Update withheld script to receive paste input from pipe

--- withheld.sh
+++ withheld.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Prints the total number of points being withheld
-ls | grep -o "_[0-9]\+-WITHHELD" | cut -c2- | cut -d "-" -f 1 | paste -sd+ | bc
+ls | grep -o "_[0-9]\+-WITHHELD" | cut -c2- | cut -d "-" -f 1 | paste -sd+ - | bc
